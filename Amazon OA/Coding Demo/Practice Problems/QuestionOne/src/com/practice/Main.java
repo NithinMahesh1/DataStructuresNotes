062eb32458ca2329143c48879e52826ebaedf090
Nithin Mahesh <nmahesh@umass.edu>
2022-04-26 14:40:34 -0400

    Adding amazon assessment screenshots and notes

---
+++ Amazon OA/Coding Demo/Practice Problems/QuestionOne/src/com/practice/Main.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Two boxes
+        // Given int arr of the item weights (arr) to be packed
+            // Divide the item weights into two subsets, A and B for packing into the associated boxes
+
+        // The intersection of A and B is null
+        // The Union A and B is equal to the original array
+        // The number of elements in subset A is minimal
+        // The sum of A's weights > the sum of B's weights
+    }
+}
