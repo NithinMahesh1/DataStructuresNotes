c98c145f1e92f5a67a91f021dc0082c7c0ef5af7
Nithin Mahesh <nmahesh@umass.edu>
2022-05-22 13:23:55 -0400

    Solution for FindSumTargets works and is O(n)!

--- LeetCode Sets/FindSumTarget/src/com/practice/Main.java
+++ LeetCode Sets/FindSumTarget/src/com/practice/Main.java
@@ -23,67 +23,31 @@ public class Main {
         int[] arr = new int[]{0,-1,2,-3,1};
         int target = -2;
 
-//        FindSumTarget(target, arr);
-        twoSum(arr,target);
-    }
-
-    public static int[] twoSum(int[] nums, int target) {
-
-        Map<Integer,Integer> map = new HashMap<>();
-        int arr[]=new int[2];
-
-        for(int i=0;i<nums.length;i++){
-            int ele = target-nums[i];
-
-            if(map.containsKey(ele)){
-                arr[0]=i;
-                arr[1]=map.get(ele);
-                break;
-            }
-            map.put(nums[i],i);
-        }
-
-        return arr;
+        FindSumTarget(target, arr);
     }
 
     /*
-        1. Use a temp int[] arr and int index (that is a pointer)
-        2. Loop through temp arr with index pointing at 0 then incrementing each time the rest of the values don't equal the target
-        3. If we find a sum in the current iteration we will add both the values to the result List and then remove them from the temp arr
-        4. If we do not find a sum with the current index we will increment the index and continue our loop
-
-
-       ALSO NOTE ONCE WE GET ONE ITERATION TO THE END OF THE TEMP ARR
+        1. Iterate array and for each element subtract it with target then check the map to see if there is another index containing this
+        2. If there is we get both elements from the map and then remove at those following indices
     */
     public static List<List<Integer>> FindSumTarget(int target, int[] arr) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> concat = new ArrayList<>();
-        int[] temp = arr.clone();
-
+        List<Integer> sums = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<temp.length; i++) {
-            map.put(i,temp[i]);
-        }
+        for(int i=0; i<arr.length; i++) {
+            int elem = target-arr[i];
 
-
-        for(int i=0; i<map.size(); i++) {
-            int j = 0;
-            for(j = i+1; j<map.size(); j++) {
-                if(map.get(i) + map.get(j) == target) {
-                    concat.add(temp[i]);
-                    concat.add(temp[j]);
-                    map.remove(i,map.get(i));
-                    map.remove(j,map.get(j));
-                    result.add(concat);
-                    concat = new ArrayList<>();
-                    break;
-                }
+            if(map.containsKey(elem)) {
+                sums.add(elem);
+                sums.add(arr[i]);
+                result.add(sums);
+                sums = new ArrayList<>();
             }
-//            i = j;
+            map.put(arr[i], i);
         }
 
-        System.out.print(result);
+        System.out.println(result);
         return result;
     }
 }
