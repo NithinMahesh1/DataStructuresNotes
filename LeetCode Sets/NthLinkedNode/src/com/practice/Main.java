97abcce7f16b625b80cf68feb4e1223987ae2d27
Nithin Mahesh <nmahesh@umass.edu>
2022-05-28 18:51:33 -0400

    Adding a very nice solution from a leetcode user for Nth Node problem set - stacks can be assigned to linkedlist!

--- LeetCode Sets/NthLinkedNode/src/com/practice/Main.java
+++ LeetCode Sets/NthLinkedNode/src/com/practice/Main.java
@@ -14,6 +14,57 @@ public class Main {
 
     */
 
+    /*
+        Note we have some edge cases such as head = [1,2]
+        Which returns [1]
+
+        This isn't really an edge case but my solution won't work for this.
+        Better solution:
+
+        Use two nodes starting with the curr at -1:
+            1. Increment all the way to the nth node for the last node
+            2. Increment all the way to the n-1th position for the prev node
+
+        Sudo Code (works in leetcode):
+
+           public ListNode removeNthFromEnd(ListNode head, int n) {
+           //base case
+        if(head == null){
+            return head;
+        }
+
+        int count = 0;
+        ListNode dummy = new ListNode(-1);
+        //Adding a dummy node befor the head, gives us the ease to write to code
+        //without having to handle seperately the case where we have to delete
+        //head node or even if there is only one node in the list
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;//ahead of slow, and heance the name
+
+        //We move the fast pointer until the count is <= n
+        while(count <= n){
+            count++;
+            fast = fast.next;
+        }
+        //Then we move slow and fast at 1x speed, until fast is null
+        //This places the slow at (n-1)th position form end
+        //As we want to remove nth node, we reset links and we remove
+        //the nth node
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //Reset the links and remove nth node
+        slow.next = slow.next.next;
+        //As head is at dummy.next, we return that
+        return dummy.next;
+    }
+
+
+                HOWEVER STACK SOLUTION BELOW WORKS WAY BETTER
+    */
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         int n = 2;
@@ -24,32 +75,68 @@ public class Main {
         list = list.insert(list,4);
         list = list.insert(list,5);
 
-        removeNthFromEnd(list.head, n);
+        removeNthFromEnd(list, n);
     }
 
-    public static LinkedList removeNthFromEnd(Node head, int n) {
-        LinkedList resultHead = new LinkedList();
-        Node curr = head;
-        Stack<Integer> stack = new Stack<>();
+//    public static LinkedList removeNthFromEnd(LinkedList head, int n) {
+//        LinkedList resultHead = new LinkedList();
+//        Node curr = head.head;
+//        int listSize = head.size(head);
+//        int removeAt = (listSize - n);
+//
+//        // Size of the linkedlist - n = the index we need to remove at
+//        // Skip and get the next node
+//        // return the list
+//
+//        if(head == null) {
+//            return resultHead;
+//        }
+//
+//        if(listSize == 1) {
+//            return resultHead;
+//        }
+//
+//
+//        int count = 0;
+//        while(curr != null) {
+//            count++;
+//            if(count == 1 && n == 1) {
+//                curr = curr.next;
+//                continue;
+//            }
+//            if(count == removeAt) {
+//                resultHead = resultHead.insert(resultHead, curr.data);
+//                curr = curr.next.next;
+//            }
+//            else {
+//                resultHead = resultHead.insert (resultHead, curr.data);
+//                curr = curr.next;
+//            }
+//        }
+//
+//
+//
+//        return resultHead;
+//    }
 
-        // Push all data to a stack
-        // get the n-1 for what index of the stack we want to pop
-        // As we pop we will start to insert to a new linked list
-        while(curr != null && curr.next != null) {
-            stack.push(curr.data);
-            curr = curr.next;
+    // This is a beautiful solution that utilizes a stack of type LinkedList
+    // We can remove the node at the index and still have all the other nodes connected at the right spot
+    public static LinkedList removeNthFromEnd(LinkedList head, int n) {
+        Node node = head.head;
+        Stack<Node> stk = new Stack<>();
+        while(node != null){
+            stk.push(node);
+            node = node.next;
         }
-
         int count = 0;
-        while(!stack.isEmpty()) {
-            if(count == n-1) {
-                continue;
-            }
-            resultHead = resultHead.insert(resultHead, stack.pop());
+        while(count < n){
+            stk.pop();
+            count++;
         }
-
-
-
-        return resultHead;
+        // This next line handles if the stack is empty (left it like this since I don't know what should go here)
+        // if(stk.isEmpty())return nodeHead.next;
+        Node node1 = stk.pop();
+        node1.next = node1.next.next;
+        return head;
     }
 }
