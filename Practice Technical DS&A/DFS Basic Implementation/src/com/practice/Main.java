ccf5a5c69711a395895c33105aeb328ae080f171
Nithin Mahesh <nmahesh@umass.edu>
2022-07-11 19:18:02 -0400

    Pushing graph implentation code - to pull from macbook

---
+++ Practice Technical DS&A/DFS Basic Implementation/src/com/practice/Main.java
@@ -0,0 +1,40 @@
+package com.practice;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Graph graph = new Graph(4);
+//
+//        graph.addEdge(0,1);
+//        graph.addEdge(0,2);
+//        graph.addEdge(1, 2);
+//        graph.addEdge(2, 0);
+//        graph.addEdge(2, 3);
+//        graph.addEdge(3, 3);
+//
+//        System.out.println(
+//                "Following is Depth First Traversal "
+//                        + "(starting from vertex 2)");
+//
+//        graph.DFS(2);
+
+        Graph g = new Graph(6);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 0);
+        g.addEdge(1, 3);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(3, 5);
+        g.addEdge(4, 3);
+        g.addEdge(5, 3);
+
+        System.out.println("Following is the Depth First Traversal");
+        g.DFS(0);
+    }
+}
