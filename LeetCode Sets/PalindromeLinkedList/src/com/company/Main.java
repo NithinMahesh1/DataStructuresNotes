b58f48d563282b57a6244a5276f3f8aff2a4325f
Nithin Mahesh <nmahesh@umass.edu>
2023-02-21 17:52:46 -0500

    This is the solution proposed by chatGPT that passes all cases, check Main.java for explanation

--- LeetCode Sets/PalindromeLinkedList/src/com/company/Main.java
+++ LeetCode Sets/PalindromeLinkedList/src/com/company/Main.java
@@ -21,46 +21,49 @@ public class Main {
     * 0 <= Node.val <= 9
     */
 
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
+    public static boolean isPalindrome(ListNode head) {
+         /**
+         * This new implementation handles cases when we run into larger inputs
+         * The idea is to use a two-pointer technique to find the middle of the linked list and then reverse the second half of the Linked List
+         * Then you can compare the first half of the Linked List with the reverse second half to determine if it is a palindrome
+         */
 
-    public static boolean isPalinrome(ListNode head) {
-        Stack<String> reverseStack = new Stack<>();
-
-        if(head == null) {
-            return false;
-        }
-
-        String concatStr = "";
-        String input = "";
-        ListNode curr = head;
-
-        while(curr != null) {
-            reverseStack.push(Integer.toString(curr.val));
-            input = input + Integer.toString(curr.val);
-            curr = curr.next;
-        }
-
-        while(!reverseStack.isEmpty()) {
-            concatStr = concatStr + reverseStack.pop();
-        }
-
-
-        if(concatStr.compareTo(input.toString()) == 0) {
+        if (head == null || head.next == null) {
             return true;
         }
 
-        return false;
+        // Find the middle of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse the second half of the linked list
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Compare the first half of the linked list with the reversed second half of the linked list
+        ListNode p1 = head;
+        ListNode p2 = prev;
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return true;
     }
 
+    // Using this helper to convert file input it ListNode list
     public static ListNode createLinkedList(int[] nodevals) {
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
@@ -77,6 +80,7 @@ public class Main {
         //ListNode head = new ListNode(1, new ListNode(2));
         ListNode head = new ListNode();
 
+        // This reads the InputExampleTestCase.txt 79/90 testcase that fails in leetcode previously
         try {
             String input = new String(Files.readAllBytes(Paths.get("src\\com\\company\\InputExampleTestCase.txt")));
             String[] inputArr = input.split("[,]"); // split input by whitespace
@@ -85,19 +89,11 @@ public class Main {
                 nodevals[i] = Integer.parseInt(inputArr[i]);
             }
             head = createLinkedList(nodevals);
-            boolean palindrome = isPalinrome(head);
+            boolean palindrome = isPalindrome(head);
             System.out.println(palindrome);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-//        ListNode head = new ListNode(nodevals[0]);
-//        ListNode curr = head;
-//
-//        for(int i=1; i<nodevals.length; i++) {
-//            curr.next = new ListNode(nodevals[i]);
-//            curr = curr.next;
-//        }
 
 //        boolean palindrome = isPalinrome(head);
 //        System.out.print(palindrome);
