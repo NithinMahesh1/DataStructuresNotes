26dfc9173533dccb519570ab47a7f0790b7f94f5
nithinmahesh1 <nmahesh.umasse.edu>
2023-03-05 18:29:10 -0500

    Finished twosum within 20 min timer and with solution passing leetcode tests

--- LeetCode Sets/TwoSum/src/com/practice/Main.java
+++ LeetCode Sets/TwoSum/src/com/practice/Main.java
@@ -1,5 +1,8 @@
 package com.practice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -22,8 +25,25 @@ public class Main {
     public static int[] twoSum(int[] sums, int target) {
         int[] result = new int[2];
 
+        Map<Integer, Integer> map = new HashMap<>();
 
+        int count = 0;
+        while(count != sums.length) {
+            //subtract current values with target and check if map contains key
+            int subtract = target-sums[count];
+            if(map.containsKey(subtract)) {
+                result[0] = map.get(subtract);
+                result[1] = count;
+            }
+            else {
+                map.put(sums[count],count);
+                count++;
+                continue;
+            }
+            count++;
+        }
 
+        System.out.println("[" +result[0] + "," +result[1] +"]");
         return result;
     }
 
@@ -31,6 +51,8 @@ public class Main {
     public static void main(String[] args) {
         int[] sums = new int[]{2,7,11,15};
         int target = 9;
+//        int[] sums = new int[]{3,2,4};
+//        int target = 6;
 
         twoSum(sums, target);
     }
