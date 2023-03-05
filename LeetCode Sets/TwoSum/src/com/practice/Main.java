6cb50e2a92bc9edbea5b2f1e5912078acf823297
Nithin Mahesh <nmahesh@umass.edu>
2023-03-05 17:33:38 -0500

    Committing twosum intial problem set

---
+++ LeetCode Sets/TwoSum/src/com/practice/Main.java
@@ -0,0 +1,37 @@
+package com.practice;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ *
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ *
+ * */
+
+public class Main {
+
+    public static int[] twoSum(int[] sums, int target) {
+        int[] result = new int[2];
+
+
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[] sums = new int[]{2,7,11,15};
+        int target = 9;
+
+        twoSum(sums, target);
+    }
+}
