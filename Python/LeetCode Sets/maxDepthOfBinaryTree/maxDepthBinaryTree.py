fb0820a84c31d143ff69cd8a15ef2c032b892686
Nithin Mahesh <nmahesh.informatics@gmail.com>
2024-02-18 16:01:04 -0500

    Added the correct solution from leetcode for maxDepth after attempting my own solution, actual solution is clean/easy to understand

--- Python/LeetCode Sets/maxDepthOfBinaryTree/maxDepthBinaryTree.py
+++ Python/LeetCode Sets/maxDepthOfBinaryTree/maxDepthBinaryTree.py
@@ -19,7 +19,6 @@
 
 # Definition for a binary tree node.
 from collections import deque
-from typing import Optional
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -28,23 +27,48 @@ class TreeNode:
         self.right = right
 
 
+# This solution is from a leetcode submission
+# Solution is actually pretty easy to understand
+# Basically using recursion we keep calling the left and right nodes adding to our counter 'depth'
+# Then we simply use the max to compare the largest depth on both left and right and return
+
 class Solution:
-    def maxDepth(self, TreeNode)-> int:
-        root = TreeNode()
-        self.queue = deque()
+    def maxDepth(self, root: TreeNode)-> int:
+        def dfs(root, depth):
+            if not root: return depth
+            return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
+                       
+        print(dfs(root,0))
+        return dfs(root, 0)
 
-        # return traverseTree(root)
-        return -1
 
 
-def createTree(tree,arr):
-    # Create a TreeNode instance to represent the root
+def createTree(arr):
+    # Set the root node in tree to arr[0]
     tree = TreeNode(arr[0])
-    # Create a deque to hold the tree nodes
-    queue = deque([tree])
-    # Index to keep track of the current position in the array
-    i = 1
 
+    # Create a queue to store the nodes and pop as we add to tree
+    queue = deque([tree])
+
+
+    # Sudo Code:
+    # i = 1
+    # while arr[i] and i < len(arr):
+    # if arr[] is not none
+        # node.left = TreeNode(arr[i])
+        # append to queue node.left
+    # i =+ 1
+
+    # if arr[] is not None and i < len(arr):
+        # node.right = TreeNode(arr[i])
+        # append to queue node.right
+    # i += 1
+
+    # return the tree
+
+
+    # [3,9,20,None,None,15,7]
+    i = 1
     while queue and i < len(arr):
         node = queue.popleft()
 
@@ -53,14 +77,20 @@ def createTree(tree,arr):
             queue.append(node.left)
         i += 1
 
+        # if arr[i] is not None and i < len(arr):
         if i < len(arr) and arr[i] is not None:
             node.right = TreeNode(arr[i])
             queue.append(node.right)
         i += 1
 
-    print(tree)
+    return tree
 
             
 values = [3,9,20,None,None,15,7]
+# values = [1,None,2]
 tree = TreeNode()
-createTree(tree,values)
+# inputTree = TreeNode(createTree(tree,values))
+inputTree = createTree(values)
+
+obj = Solution()
+obj.maxDepth(inputTree)
