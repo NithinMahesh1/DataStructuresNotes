08cea87092d02f5f00af300831b5e6f549605b15
NithinMahesh1 <nmahesh.informatics@gmail.com>
2024-10-20 15:12:40 -0400

    Used counter in python and was able to sort the list by most freq then append to a new list and return

--- Python/NeetCode Blind 75/Arrays and Hashing/topKFrequent.py
+++ Python/NeetCode Blind 75/Arrays and Hashing/topKFrequent.py
@@ -14,43 +14,27 @@
 # Input: nums = [1,2,2,3,3,3,4,4,5], k = 3
 # Output: [2,3,4]
 
+from collections import Counter
+
 def topKFrequent(k,nums) -> list:
-    # Loop arr and add arr items to key and val as k = 1,2,3
-    dict = {}
-    list = []
+	# Use the collections Counter instead
+    # Find the most frequent and figure out how to remove the excess
+	sortedList = Counter(nums).most_common(k)
+	list = []
+	
+	for vals in sortedList:
+		list.append(vals[0])
+	
+	return list
 
-    if(len(nums) == 1):
-        return [nums[0]]
-
-    if(len(nums) == 0):
-        return [0]
-
-    i = 0
-    for num in nums:
-        if(k == 0):
-            return list
-        if(len(dict) == 0):
-            dict[num] = k
-            continue 
-        if(num in dict.keys()):
-            dict[num] = k
-            if(dict.get(num) == 1):
-                continue
-            k = k - 1
-            list.append(num)
-        else:
-            dict[num] = k
-        i += 1
-
-    return list
 
 def main():
-    # k = 2
-    # nums = [1,2,2,3,3,3]
+    k = 2
+    nums = [1,2,2,3,3,3]
     # nums = [7,7]
     # k = 1
-    nums = [1,2,2,3,3,3,4,4,5]
-    k = 3
+    # nums = [1,2,2,3,3,3,4,4,5]
+    # k = 3
     print(topKFrequent(k,nums))
 
 main()
