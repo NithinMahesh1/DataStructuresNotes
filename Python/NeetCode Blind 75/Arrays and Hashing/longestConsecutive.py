8486c3b38eeb9ccaaa33af65df3192fadf31294d
Nithin Mahesh <NMahesh-da@NEAEG.local>
2024-11-08 11:28:46 -0500

    Starter files for longestConsecutive medium problem

---
+++ Python/NeetCode Blind 75/Arrays and Hashing/longestConsecutive.py
@@ -0,0 +1,30 @@
+# Given an array of integers nums, return the length of the longest consecutive sequence of elements.
+
+# A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element.
+
+# You must write an algorithm that runs in O(n) time.
+
+
+# Example 1:
+# Input: nums = [2,20,4,10,3,4,5]
+# Output: 4
+# Explanation: The longest consecutive sequence is [2, 3, 4, 5].
+
+# Example 2:
+# Input: nums = [0,3,2,5,4,6,1,1]
+# Output: 7
+
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+
+        return []
+
+    def run(self):
+        nums = [2,20,4,10,3,4,5]
+        self.longestConsecutive(nums)
+
+def main():
+    solution = Solution()
+    solution.run()
+
+main()
