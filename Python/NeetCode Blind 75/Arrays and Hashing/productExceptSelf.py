703edf27a65f30e110d966c2b26443b44e868dad
NithinMahesh1 <nmahesh.informatics@gmail.com>
2024-11-07 20:11:46 -0500

    producteExceptSelf problem set solution via neetcode, need to use postfix and prefix to modify one array to return solution

--- Python/NeetCode Blind 75/Arrays and Hashing/productExceptSelf.py
+++ Python/NeetCode Blind 75/Arrays and Hashing/productExceptSelf.py
@@ -18,15 +18,40 @@
 # Explanation:
 # We need to multiply all the values in the arr except for curr index
 # Append those values to a new arr
+import numpy as np
+
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        # Multiply all the vals
-        # but somehow without division exclude the curr val
+        # https://www.youtube.com/watch?v=bNvIQI2wAjk&ab_channel=NeetCode
+        # Use prefix and postfix
+        # Basically we multiply all the values in prefix before each curr in a loop
+        # Then we loop backwards and multiply all the values with prefix at that point in a loop
+        # Then return the new res
+
+        # Intialiaze an arr of each position of 1 and multiplied by the length
+        res = [1] * len(nums)
         
-        return []
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            # The following is essentially using the same arr
+            # That array already contains the prefix values
+            # So we are multiplying the postfix points after curr val
+            # by the prefix at that index in the array
+            res[i] *= postfix
+            # We have to continuously update the postfix
+            # Multiply it by the current value in nums
+            postfix *= nums[i]
+
+        return res
 
     def run(self):
         nums = [1,2,4,6]
+        # nums=[-1,0,1,2,3]
         print(self.productExceptSelf(nums))
 
 
