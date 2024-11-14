dd971f310ace8e5fc9508e5b4b061608476a73d4
NithinMahesh1 <nmahesh.informatics@gmail.com>
2024-11-14 18:49:52 -0500

    Finished my solution for twoSumII which does not pass all test cases up to 20/24 - going to look up solution for this

---
+++ Python/NeetCode Blind 75/Two Pointers/PreReqs/TwoSumII.py
@@ -0,0 +1,65 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+# find two numbers such that they add up to a specific target number. 
+# Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+# Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+# The tests are generated such that there is exactly one solution. 
+# You may not use the same element twice.
+
+# Your solution must use only constant extra space.
+
+# Example 1:
+# Input: numbers = [2,7,11,15], target = 9
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+# Example 2:
+# Input: numbers = [2,3,4], target = 6
+# Output: [1,3]
+# Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+# Example 3:
+# Input: numbers = [-1,0], target = -1
+# Output: [1,2]
+# Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+import numpy
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        curr = 0
+        iter = curr + 1
+        while iter < len(numbers):
+            if(numbers[curr] + numbers[iter] == target):
+                return [curr+1,iter+1]
+            i = iter
+            for i in range(i,len(numbers),1):
+                if(numbers[curr] + numbers[i] == target):
+                    # we do the adding again to see if we should return
+                    return [curr+1, i+1]
+            # If that above did not work we increment curr and iter
+            if(curr < len(numbers)):
+                curr += 1
+                iter += 1
+        
+        return []
+
+
+
+    def run(self):
+        numbers = [2,7,11,15]
+        target = 9
+        # numbers = [2,3,4]
+        # target = 6
+        # numbers = [-1,0]
+        # target = -1
+        # numbers = [5,25,75]
+        # target = 100
+        print(self.twoSum(numbers, target))
+
+
+def main():
+    solution = Solution()
+    solution.run()
+
+main()
