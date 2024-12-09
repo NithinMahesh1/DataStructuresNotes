55c0c02d34adaa3e34d5fc51a264950ce770a6b3
Nithin Mahesh <NMahesh-da@NEAEG.local>
2024-12-09 17:04:38 -0500

    Committing my logic for minWindow problem and start to a solution

--- Python/NeetCode Blind 75/Sliding Window/minWindow.py
+++ Python/NeetCode Blind 75/Sliding Window/minWindow.py
@@ -19,8 +19,24 @@
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
-        
+        # L and R pointer window
+        # Start L at first occurence of t val
+        # Once we get there increment R until all of t vals are touched
+        # Take the min of the window as res so res = min(res,win)
+        # Keep looping until R is at last val
+        # Increment L to next occurence of t val once all vals are hit in last round
+        l = 0, r = 0
+        res = 0
+        win = 0
+        for l in range(len(s)):
+            if(s[l] in t):
+                # loop here from L and R = L + 1
+                r = l + 1
+                while r < len(s):
+                    # increment r here until it reaches 2 more t vals
+            l += 1
+
+
 
 def main():
     s = "OUZODYXAZV"
