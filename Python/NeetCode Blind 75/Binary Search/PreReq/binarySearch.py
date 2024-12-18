a49b71a01bcc0ee1d10e31ad839ad5c6fb0e26b8
Nithin Mahesh <NMahesh-da@NEAEG.local>
2024-12-18 16:48:05 -0500

    Correct proper logic for binary search normal implementation

--- Python/NeetCode Blind 75/Binary Search/PreReq/binarySearch.py
+++ Python/NeetCode Blind 75/Binary Search/PreReq/binarySearch.py
@@ -1,32 +1,31 @@
 class Solution:
 	def binarySearch(self, nums: list[int], n: int) -> int:
-		# Essentially is mid = (L+R)/2
-		# If n < mid:
-		# 	R = mid
-		# If n > mid:
-		# 	L = mid
-		# If n == mid:
-		# 	return mid
-		
-		mid = 0
-		l,r = 0,len(nums)-1
-		while(mid != n):
-			mid = int((nums[l]+nums[r])/2)
-			if(n == mid):
-				return mid
-			if(n < mid):
-				r = mid
-			if(n > mid):
-				l = mid
-			
-		return 0
+		# If mid is at target return
+		# If mid val is > than target we make R less than mid by one
+			# this is because it should be the left half checked
+		# Otherwise if it is mid val is less than n we should check right
+			# that is why we increase L to mid
+		L,R = 0,len(nums)-1
+		while L <= R:
+			mid = (L+R)//2
+			if(nums[mid] == n):
+				print(nums[mid])
+				return nums[mid]
+			if(nums[mid] > n):
+				R = mid - 1
+			else:
+				L = mid + 1
+
+		return -1
 
 
 def main():
 	solution = Solution()
 	# nums=[1,2,3,4,5]
-	nums = [1,2,3,4,5,6,7,8,9]
-	n=2
+	# nums = [1,2,3,4,5,6,7,8,9]
+	# n=2
+	nums = [1, 3, 5, 7, 9, 11]
+	n = 7
 	solution.binarySearch(nums,n)
 
 
