32e07fd94b21992f8fab93f83dd8578f0b56a4fa
NithinMahesh1 <nmahesh.informatics@gmail.com>
2025-01-02 11:17:58 -0500

    Starting trees, I have the right idea, but this can be done easily with recursion

---
+++ Python/NeetCode Blind 75/Trees/invertTree.py
@@ -0,0 +1,69 @@
+# You are given the root of a binary tree root. Invert the binary tree and return its root.
+
+# Input: root = [1,2,3,4,5,6,7]
+# Output: [1,3,2,7,6,5,4]
+
+# Input: root = [3,2,1]
+# Output: [3,1,2]
+
+# Example 3:
+# Input: root = []
+# Output: []
+
+from typing import Optional
+from collections import deque
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # Need to use DFS algorithm
+        # We can use inorder traversal since we want to swap left and right
+        # Start at root -> left -> right traversal using DFS
+        start = root
+        curr = TreeNode(start)
+
+        while curr:
+            left = curr.left
+            right = curr.right
+
+            curr.left = right
+            curr.right = left
+
+            curr = curr.left or curr.right
+            
+
+        return start
+
+    def buildTree(self, arr):
+        tree = TreeNode(arr[0])
+        queue = deque([tree])
+
+        i = 1
+        while queue != None and i < len(arr):
+            node  = queue.popleft()
+
+            if(arr[i] is not None):
+                node.left = TreeNode(arr[i])
+                queue.append(node.left)
+            i += 1
+
+            if(arr[i] is not None and i < len(arr)):
+                node.right = TreeNode(arr[i])
+                queue.append(node.right)
+            i += 1
+
+        return tree
+
+def main():
+    root = [1,2,3,4,5,6,7]
+    solution = Solution()
+    root = solution.buildTree(root)
+    solution.invertTree(root)
+
+
+main()
