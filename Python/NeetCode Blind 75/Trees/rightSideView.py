5b1bcb01f75583401f06ab7cbd25e1f3244ff261
NithinMahesh1 <nmahesh.informatics@gmail.com>
2025-01-19 15:56:48 -0500

    Read rightSideView tree problem description wrong and solved it wrong - pushing solution soon

---
+++ Python/NeetCode Blind 75/Trees/rightSideView.py
@@ -0,0 +1,72 @@
+# You are given the root of a binary tree.
+# Return only the values of the nodes that are visible from the right side of the tree,
+# ordered from top to bottom.
+
+# Example 1:
+# Input: root = [1,2,3]
+# Output: [1,3]
+
+# Example 2:
+# Input: root = [1,2,3,4,5,6,7]
+# Output: [1,3,7]
+
+from typing import Optional
+from collections import deque
+# Definition for a binary tree node.
+class TreeNode:        
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> list[int]:
+        # I think for this problem we need to use dfs 
+        # but simply only traverse the right subtree
+        if(root is None):
+            return []
+        node = root
+        res = [node.val]
+
+        def dfs(node,res):            
+            if node is None:
+                return res
+            res.append(node.val)                            
+            return dfs(node.right,res) or dfs(node.left, res)
+        
+        return dfs(node.right,res)
+        
+
+    def buildTrees(self, arr) -> TreeNode:
+        if arr is None:
+            return TreeNode()
+
+        root = TreeNode(arr[0])
+        queue = deque([root])
+
+        i = 1
+        while queue and i < len(arr):
+            node = queue.popleft()
+
+            if(arr[i] is not None):
+                node.left = TreeNode(arr[i])
+                queue.append(node.left)
+            i += 1
+
+            if(i < len(arr) and arr[i] is not None):
+                node.right = TreeNode(arr[i])
+                queue.append(node.right)
+            i += 1
+
+        return root
+
+
+def main():
+    solution = Solution()
+    # root = [1,2,3]
+    # root = [1,2,3,4,5,6,7]
+    # root=[]
+    root=[1,2]
+    solution.rightSideView(solution.buildTrees(root))
+
+main()
