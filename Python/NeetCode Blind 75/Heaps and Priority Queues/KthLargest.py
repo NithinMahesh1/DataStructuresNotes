bfa5fb6e3430ab3160b80d6e8a97775dbd5b9ebd
NithinMahesh1 <nmahesh.informatics@gmail.com>
2025-01-20 20:11:54 -0500

    Heaps easy problem kthlargest completed

--- Python/NeetCode Blind 75/Heaps and Priority Queues/KthLargest.py
+++ Python/NeetCode Blind 75/Heaps and Priority Queues/KthLargest.py
@@ -21,16 +21,38 @@
 # kthLargest.add(7);   // return 5
 # kthLargest.add(8);   // return 6
 
-
+import heapq
 class KthLargest:
-
     def __init__(self, k: int, nums: list[int]):
-        
+        self.k = k
+        self.heap = []
+        for num in nums:
+            self.add(num)
 
     def add(self, val: int) -> int:
+        # Adding the new value to the heap
+        heapq.heappush(self.heap,val)
+
+        # If the heap gets bigger than the len of k,
+        # we remove the smallest element
+        if(len(self.heap) > self.k):
+            heapq.heappop(self.heap)
+        
+        # The first value will always be the min value
+        # Since we keep the heap to always be of size k
+        # We pop if the len gets larger and keep the greatest values at the end
+        # This way we always pop and return the kth smallest
+        return self.heap[0]
 
 
-def main()
+def main():
+    k = 3
+    nums = [1,2,3,3]
+    obj = KthLargest(k,nums)
+    print(obj.add(3))
+    print(obj.add(3))
+    print(obj.add(3))
+    print(obj.add(5))
+    print(obj.add(6))
     
-
 main()
