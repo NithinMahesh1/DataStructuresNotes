ae0ad66da21b6ff90d95f2b45fb237d387669ac6
NithinMahesh1 <nmahesh.informatics@gmail.com>
2025-01-30 20:19:01 -0500

    Possible questions to practice at some point

--- Python/NeetCode Blind 75/Misc/g.py
+++ Python/NeetCode Blind 75/Misc/g.py
@@ -1,16 +1,37 @@
-import random
 # Python homework - write a random number generator. Write some SQL statements.
+# This solution is the easy way
+import random
 class Solution:
     def ranGen(self) -> int:
-        return random.randomrange(0,float("inf"))
+        return int(random.uniform(0,int(1e9)))
 
 def main():
     solution = Solution()
     print(solution.ranGen())
 
+# Just in case this is the C# solution as well:
+# using System
+# namespace gen
+# {
+#     public class gen {
+#         public static int ranGen() {
+#             Random random = new Random();
+#             int randomNum = random.Next();
+#             return randomNum;
+#         }
+#         public static void Main(string[] args) {
+#             Console.WriteLine(ranGen());
+#         }
+#     }
+# }
+
+
+# Implementation from scratch
+# Python:
 
 
 # How do you make a class immutable
+
 # What is immutable vs mutable
 # How does garbage collection work in Python?
 # What is an access modifier
---
+++ Python/NeetCode Blind 75/Misc/ranGen.cs
@@ -0,0 +1,15 @@
+using System
+
+namespace gen
+{
+    public class gen {
+        public static int ranGen() {
+            Random random = new Random();
+            int randomNum = random.Next();
+            return randomNum;
+        }
+        public static void Main(string[] args) {
+            Console.WriteLine(ranGen());
+        }
+    }
+}
