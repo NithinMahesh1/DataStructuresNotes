bb2fca333a10d6c04269ea9fbe79df4a3a6949d9
NithinMahesh1 <nmahesh.informatics@gmail.com>
2025-02-01 17:04:48 -0500

    Finished updating notes for MISC and pushing final notes

--- Python/NeetCode Blind 75/Misc/g.py
+++ Python/NeetCode Blind 75/Misc/g.py
@@ -35,11 +35,15 @@ def main():
 # It's deterministic, meaning if we set the same seed, it will produce the same sequence of numbers. 
 # It typically uses a Linear Congruential Generator (LCG)
 
-# How do you make a class immutable
-
+# How do you make a class immutable?:
+# You can make them immutable in a few different ways, one can be using "readonly" field for private fields
+# In custom classes we can also set "get" and not "set" for properties
+# We can also set all fields in the constructor
+# IEnumerable example, lazy loading (elems only computed or iterated when needed e.g. foreach loops) and how it is memory efficient
 
 # What is immutable vs mutable?:
-# Mutable vs immutable is that immuatble classes are classes that cannot be modified after creation.
+# Immutability is basically a class where it's state cannot be changed after its creation
+# Mutable vs immutable is that immutable classes are classes that cannot be modified after creation.
 # All fields must be read-only after initialization
 # No setters or mutators should exist
 # If a class contains collections they should be read only
@@ -70,11 +74,47 @@ def main():
 
 
 # How does garbage collection work in Python and C#?
+# C# uses the CLR (Common Language Runtime), 
+# which is basically the equivalent of the JVM and allows all .net languages to be run on it (c#, VB, etc).
+# All .NET languages compile to CLR bytecode. CLR provides system level functionality like garbage collection.
+# CLR runs the compiled code
+# There are two ways it works: heaps (for creating objects new) and stacks (for simple data types and references)
+# Mainly runs on a managed heap:
+# When we initialize a new process some an address of space is allocated, this is the managed heap.
+# It maintains a pointer to the address where to object in the heap will be allocated
+# As we create new objects the runtime allocates memory for the address space following the first object
+# We then release memory when the garbage collector performs a collection. Basically releases objects 
+# that are no longer being used by the application.
+# It chooses these to remove based off of objects in use, if we can't reach memory is freed, and if we are able to
+# those objects are moved to compact memory and pointers updated, heap pointer to last reachable object
+# Geeks for Geeks explanation:  
+
+# Marking Phase: A list of all the live objects is created during the marking phase. 
+# This is done by following the references from all the root objects. 
+# All of the objects that are not on the list of live objects are potentially deleted from the heap memory.
+
+# Relocating Phase: The references of all the objects that were on the list of 
+# all the live objects are updated in the relocating phase so that they point to the new location 
+# where the objects will be relocated to in the compacting phase.
+
+# Compacting Phase: The heap gets compacted in the compacting phase as the space occupied by the dead objects is released and the live objects remaining are moved. 
+# All the live objects that remain after the garbage collection are moved towards the older end of the heap memory in their original order. 
+
+# Which objects will be garbage collected
+# Answered above but basically objects that are unreachable from the application roots
+# Check the managed heap looking for blocks of address space occupied by unreachable objects
+# Remove these the compact the managed heap
 
 
 # What is an access modifier
-# Which objects will be garbage collected
-# http://geeksforgeeks.org/garbage-collection-python/
-
+# This is important to Encapsulation and Inheritance
+# public, private, protected, internal
+# public: accessible anywhere in the program
+# private: accessible only in the current class
+# protected: accessible in same class and derived classes
+# internal: accessible in only the same assembly or program
 
 main()
+
+# Misc:
+# http://geeksforgeeks.org/garbage-collection-python/
