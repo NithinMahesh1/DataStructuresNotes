0335b8f7af7648b169626f27ed87a68c217aebef
NithinMahesh1 <nmahesh.informatics@gmail.com>
2025-02-10 21:12:37 -0500

    Still resolving bugs on isAlienSorted graph set problem

--- C#/NeetCode Blind 150/Graphs/IsAlienSorted.cs
+++ C#/NeetCode Blind 150/Graphs/IsAlienSorted.cs
@@ -25,6 +25,34 @@
 
 public class Solution {
     public bool IsAlienSorted(string[] words, string order) {
+        // Basically two conditions:
+        // One is that the if w1 is < w2 then true if > then false
+        // The other is w1 letter must come before w2 first letter and so forth
+        // We can use a hashset to loop the string order
         
+        Dictionary<char,int> alienDict = new Dictionary<char,int>();
+        int z = 0;
+        foreach(char s in order) {
+            alienDict[s] = z;
+        }
+
+        // Loop the list of words
+        for(int i=0; i<words.Length; i++) {
+            string w1 = words[i];
+            string w2 = words[i+1];
+            // Loop the chars in the words
+            for(int j=0; j<w1.Length; j++) {
+                if(j == w2.Length) {
+                    // This is because it means 
+                    // we are still looping in bounds of w1
+                    return false;
+                }
+                if(alienDict[w1[j]] > alienDict[w2[j]]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
