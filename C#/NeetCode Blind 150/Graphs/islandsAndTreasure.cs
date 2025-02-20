b6c81127b0ffca2ce02045930a2f1cf5689da340
NithinMahesh1 <nmahesh.informatics@gmail.com>
2025-02-20 18:34:59 -0500

    Missed some important syntax errors mainly being the size of the queue being changed when we loop

--- C#/NeetCode Blind 150/Graphs/islandsAndTreasure.cs
+++ C#/NeetCode Blind 150/Graphs/islandsAndTreasure.cs
@@ -50,52 +50,55 @@ public class Solution {
     }
     public void islandsAndTreasure(int[][] grid) {
         // In problems where we are trying to find distance to something we should be using BFS
-        // We also need a queue set to keep track of the treasure chests and a hashset to keep track of the visited cell
+        // We also need a queue set to keep track of the treasure chests and a hashset to keep track of the visited cells
         // Proper way to do this is using the treasure chest's "0" as the starting points
         // From there we should count and replace the 2147483647 values with the distance from the 0
-        Queue<(int,int)> queue = new Queue<(int,int)>();
-        HashSet<(int,int)> visited = new HashSet<(int,int)>();
+        
+        Queue<(int, int)> queue = new Queue<(int, int)>();
+        HashSet<(int, int)> visited = new HashSet<(int, int)>();
 
         // Loop to find the treasure cells first
-        for(int i=0; i<grid.Length; i++) {
-            for(int j=0; j<grid[i].Length; j++) {
-                if(grid[i][j] == 0) {
-                    queue.Enqueue((i,j));
-                    visited.Add((i,j));
+        for (int i = 0; i < grid.Length; i++) {
+            for (int j = 0; j < grid[i].Length; j++) {
+                if (grid[i][j] == 0) {
+                    queue.Enqueue((i, j));
+                    visited.Add((i, j));
                 }
             }
         }
 
         int dist = 0;
         // Start a while loop from the treasure chests and start setting the distances        
-        while(queue.Count != 0) {
-            // Get grid position from queue
-            for(int z=0; z<queue.Count; z++) {
-                // We are popping the the treasure chests from the queue 
-                var (i,j) = queue.Dequeue();
-                
-                // For each treasure chest we are changing to be the current distance
-                // We end up updating the distance each iteration of treasure chests from queue
+        while (queue.Count != 0) {
+            // Number of elements in the current level
+            // Need to update since we are updating the size through the loop
+            int size = queue.Count; 
+
+            for (int z = 0; z < size; z++) {
+                var (i, j) = queue.Dequeue();
+
+                // Update grid with current distance
                 grid[i][j] = dist;
 
-                // Increment the positions of the grid (traversal)
-                traverseGrid(grid,i+1,j,visited,queue);
-                traverseGrid(grid,i-1,j,visited,queue);
-                traverseGrid(grid,i,j+1,visited,queue);
-                traverseGrid(grid,i,j-1,visited,queue);
+                // Traverse adjacent cells
+                traverseGrid(grid, i + 1, j, visited, queue);
+                traverseGrid(grid, i - 1, j, visited, queue);
+                traverseGrid(grid, i, j + 1, visited, queue);
+                traverseGrid(grid, i, j - 1, visited, queue);
             }
-            // Here we increment the distance for each cell from the treasure chest
+            // Increase the distance for the next level of BFS
             dist += 1;
         }
-
     }
-    private void traverseGrid(int[][] grid, int i, int j, HashSet<(int,int)> visited, Queue<(int,int)> queue) {
-        if(i < 0 || i >= grid.Length || j < 0 || j >= grid[i].Length || grid[i][j] == -1) {
+
+    private void traverseGrid(int[][] grid, int i, int j, HashSet<(int, int)> visited, Queue<(int, int)> queue) {
+        // Check if out of bounds or blocked (-1)
+        if (i < 0 || i >= grid.Length || j < 0 || j >= grid[i].Length || grid[i][j] == -1 || visited.Contains((i, j))) {
             return;
         }
 
-        // Mark it as visited
-        visited.Add((i,j));
-        queue.Enqueue((i,j));
+        // Mark as visited and enqueue for processing
+        visited.Add((i, j));
+        queue.Enqueue((i, j));
     }
 }
