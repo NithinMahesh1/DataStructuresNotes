2e44c29ba86fb0b61b506b66a245ac944cec400c
NithinMahesh1 <nmahesh.informatics@gmail.com>
2025-02-27 16:29:42 -0500

    Another attempt at course map - was in the middle of it, pushing to continue on desktop

--- C#/Sandbox/Program.cs
+++ C#/Sandbox/Program.cs
@@ -18,42 +18,27 @@ public class Solution {
         obj.CanFinish(numCourses,prerequisites);
     }
     public bool CanFinish(int numCourses, int[][] prerequisites) {
-        // Need to detect if it is a cycle and if it is return false
-        // e.g. 0,1 and 1,0 is 0 -> 1 -> 0
-        // Use a hash map to add the courses as key and the vals as preqs
-        // Iterate through and use a visit hashset - this will show us if our graph returns to a visited cell
-        // means we return false and a cycle was detected
-
         Dictionary<int,List<int>> courseMap = new Dictionary<int, List<int>>();
         HashSet<int> visited = new HashSet<int>();
 
-        // Loop through and populate courseMap
-        foreach(int[] course in prerequisites) {
-            if(!courseMap.ContainsKey(course[0])) {
-                courseMap[course[1]] = new List<int>{ course[0] };
+        // First we will need to loop through and add all courses to Dict
+        foreach(int[] courses in prerequisites) {
+            if(!courseMap.ContainsKey(courses[0])) {
+                courseMap[courses[0]] = new List<int>();
             }
-            else {
-                List<int> templist = courseMap[course[1]];
-                templist.Add(course[0]);
-            }
+            List<int> templist = courseMap[courses[0]];
+            courseMap[courses[0]] = templist;
         }
 
-        // Utilize dfs to traverse using the courseMap
-        dfs(courseMap,visited);
+        // Loop the courseMap and use dfs to check the prereq courses
+        // We need to check which ones have prereqs valid before as well
+        for(int c=0; c<courseMap.Count; c++) {
+            if(courseMap[c] == ) {
+
+            }
+
+        }
+
         return false;
-    }
-    private bool dfs(Dictionary<int,List<int>> courseMap, HashSet<int> visited) {
-        // Need to loop dictionary keys then dfs their values
-        // If we get back to one that is visited we return false
-        for(int i=0; i<courseMap.Keys.Count; i++) {
-            if(visited.Contains(i)) {
-                return false;
-            }
-            dfs();
-
-            visited.Add(courseMap.FirstOrDefault(cs => cs.Value == courseMap[i]).Key);
-        }
-
-        return true;
     }
 }
