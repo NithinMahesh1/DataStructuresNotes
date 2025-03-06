fbe82fb02ffe6fe6e9065ef3c5b10ca717be8ef4
Nithin Mahesh <NMahesh-da@NEAEG.local>
2025-03-06 16:22:32 -0500

    Comitting start of attempt at findredundantconnection graph problem

--- C#/Sandbox/Program.cs
+++ C#/Sandbox/Program.cs
@@ -1,99 +1,89 @@
-﻿// There is an undirected graph with n nodes. There is also an edges array, where edges[i] = [a, b] 
-// means that there is an edge between node a and node b in the graph.
+﻿// You are given a connected undirected graph with n nodes labeled from 1 to n. Initially, 
+// it contained no cycles and consisted of n-1 edges.
 
-// The nodes are numbered from 0 to n - 1.
+// We have now added one additional edge to the graph. 
+// The edge has two different vertices chosen from 1 to n, 
+// and was not an edge that previously existed in the graph.
 
-// Return the total number of connected components in that graph.
+// The graph is represented as an array edges of length n where edges[i] = [ai, bi] 
+// represents an edge between nodes ai and bi in the graph.
+
+// Return an edge that can be removed so that the graph is still a connected non-cyclical graph. 
+// If there are multiple answers, return the edge that appears last in the input edges.
 
 // Example 1:
-// Input:
-// n=3
-// edges=[[0,1], [0,2]]
-// Output:
-// 1
+// Input: edges = [[1,2],[1,3],[3,4],[2,4]]
+// Output: [2,4]
 
 // Example 2:
-// Input:
-// n=6
-// edges=[[0,1], [1,2], [2,3], [4,5]]
-// Output:
-// 2
+// Input: edges = [[1,2],[1,3],[1,4],[3,4],[4,5]]
+// Output: [3,4]
+
 public class Solution {
     public int count = 0;
     public static void Main(string[] args) {
-        // int n = 6;
-        // int[][] edges = new int[][]{
-        //     new int[]{0,1},
-        //     new int[]{1,2},
-        //     new int[]{2,3},
-        //     new int[]{4,5}
-        // };
-
-        int n = 3;
         int[][] edges = new int[][]{
-            new int[]{0,1},
-            new int[]{0,2}
+            new int[]{1,2},
+            new int[]{1,3},
+            new int[]{3,4},
+            new int[]{2,4}
         };
 
         Solution obj = new Solution();
-        obj.CountComponents(n,edges);
+        obj.FindRedundantConnection(edges);
     }
 
-    public int CountComponents(int n, int[][] edges) {
-        // Loop through and add all values to a dictionary first
+    public int[] FindRedundantConnection(int[][] edges) {
+        // Basically we need to detect a cycle in the graph
+        // Once we detect a cycle (we can keep appending this to a path)
+        // we will then remove the last edge in that cycle
+        // Thats why in example 2 we would elimate 3,4 since that is the end of the cycle
         Dictionary<int,List<int>> edgeDict = new Dictionary<int, List<int>>();
         HashSet<int> visited = new HashSet<int>();
-        HashSet<int> path = new HashSet<int>();
+        // List<int> removedEdge = new List<int>();
 
-        for(int i=0; i<n; i++) {
-            edgeDict[i] = new List<int>();
-        }
-        
-        foreach(int[] pair in edges) {
-            int edge1 = pair[0];
-            int edge2 = pair[1];
-            edgeDict[edge1].Add(edge2);
-            edgeDict[edge2].Add(edge1);
-        }
+        // Initialize empty dictionary with list values to keep track of nodes and their
+        // associated edges
+        // for(int i=0; i<edges.Length; i++) {
+        //     edgeDict[i] = new List<int>();
+        // }
 
-        for(int e=0; e<n; e++) {
-            if(!dfs(e,edgeDict,visited,path)) {
-                continue;
+        // // Loop each value and append each edge to the dict
+        // foreach(int[] pair in edges) {
+        //     int node1 = pair[0];
+        //     int node2 = pair[1];
+        //     edgeDict[node1].Add(node1);
+        //     edgeDict[node2].Add(node2);
+        // }
+
+        for(int e=0; e<edges.Length; e++) {
+            int[] pair = edges[e];
+            int node1 = pair[0];
+            int node2 = pair[1];
+            if(dfs(e,-1,edgeDict,visited)) {
+                return new int[]{};
             }
         }
 
-        return count;
+        // Loop now initialized dict and run dfs
+        return new int[]{};
     }
-    private bool dfs(int e, Dictionary<int,List<int>> edgeDict, HashSet<int> visited, HashSet<int> path) {
-        // If the path contains edge we return false and end recursion
-        if(path.Contains(e)) {
-            return false;
-        }
-
-        // If visited contains edge return true since we visited already but 
-        // need to keep iterating
+    private bool dfs(int e, int prev, Dictionary<int,List<int>> edgeDict, HashSet<int> visited) {
         if(visited.Contains(e)) {
             return true;
         }
 
-        // Add to path
-        path.Add(e);
+        visited.Add(e);
 
-        // Call dfs in a loop to continue checking adjacent edges
-        foreach(int edge in edgeDict[e]) {
-            if(!dfs(edge,edgeDict,visited,path)) {
+        foreach(int node in edgeDict[e]) {
+            if(node == prev) {
                 continue;
+            }
+            if(dfs(node,e,edgeDict,visited)) {
+                return true;
             }
         }
 
-        // Remove from path
-        path.Remove(e);
-        if(path.Count == 0) {
-            count++;
-        }
-        // Add to Visited after
-        visited.Add(e);
-
-        return true;
+        return false;
     }
 }
