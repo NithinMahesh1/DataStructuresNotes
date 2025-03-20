51aed6aa718dd2a2e05a95b13ba7b3e56c286a2e
Nithin Mahesh <NMahesh-da@NEAEG.local>
2025-03-20 10:41:52 -0400

    Finished solving linked list find duplicates questions (basically was a array question not linked list)

--- C#/NeetCode Blind 150/LinkedLists/findDuplicate.cs
+++ C#/NeetCode Blind 150/LinkedLists/findDuplicate.cs
@@ -1,4 +1,4 @@
-﻿// You are given an array of integers nums containing n + 1 integers. 
+// You are given an array of integers nums containing n + 1 integers. 
 // Each integer in nums is in the range [1, n] inclusive.
 
 // Every integer appears exactly once, except for one integer which appears two or more times. 
